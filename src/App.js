1ebd99078c16cda4eab1a032f6a29462eb7d0581
Amir Shahzad <amirshahzad.t7@gmail.com>
2023-02-02 14:46:56 +0500

    Update About Section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import HeroSection from "./Components/Hero Section/HeroSection";
 import NavBar from "./Components/NavBar/NavBar";
 import "./App.css";
-import About from "./Components/About Section/About";
 
 const App = (React.FC = () => {
   return (
@@ -11,7 +10,6 @@ const App = (React.FC = () => {
         <NavBar />
         <HeroSection />
       </div>
-      <About />
     </div>
   );
 });
