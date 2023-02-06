9cde12aecf4d9fd92343596fa1b5efdeadefb097
Amir Shahzad <amirshahzad.t7@gmail.com>
2023-02-06 17:25:24 +0500

    Experties Section

--- src/Components/Experties/Experties.js
+++ src/Components/Experties/Experties.js
@@ -2,7 +2,13 @@ import React from "react";
 import "../Experties/Experties.css";
 
 const Experties = () => {
-  return <div></div>;
+  return (
+    <div className="experties-section pt-5">
+      <div className="container">
+        <h2 className="experties-heading">Experties</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Experties;
